19f17f3351a9228e22f7abe914815bf64bec97e5
daph <daphneyapjy@hotmail.com>
2023-03-17 19:14:21 +0000

    implemented a chat interface

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import openai
 from model import GeneralModel
 import streamlit as st
 from functions import convert_pdf_to_txt_pages, convert_pdf_to_txt_file, save_pages, displayPDF, images_to_txt
-
+from streamlit_chat import message
 
 html_temp = """
             <div style="background-color:{};padding:1px">
@@ -16,25 +16,52 @@ languages = {
     'Spanish': 'spa',
 }
 
-def app():
+
+def app(user_input=None):
     # Creating an object of prediction service
     pred = GeneralModel()
 
-
     st.title("Compass AI 🧭")
     st.caption("Transform your academic, professional, and personal "
-                 "development with individualized AI-powered guidance!")
+               "development with individualized AI-powered guidance!")
+
+    # initialize chat if user doesn't have a session
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hi! I am an AI designed to help you with your career directions. Ask me "
+                                         "anything!"]
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hello!"]
+    if 'chat_displayed' not in st.session_state:
+        st.session_state['chat_displayed'] = False
+
     # Using the streamlit cache
     @st.cache
-    def process_prompt(input, cv=False, career=False, edu=False, pd=False):
+    def process_prompt(input, cv=False):
         if cv:
             return pred.model_prediction(input=input.strip(), api_key=api_key, cv=True)
-        elif career:
-            return pred.model_prediction(input=input.strip(), api_key=api_key, career=True)
-        elif edu:
-            return pred.model_prediction(input=input.strip(), api_key=api_key, edu=True)
-        elif pd:
-            return pred.model_prediction(input=input.strip(), api_key=api_key, pd=True)
+        return pred.model_prediction(input=input.strip(), api_key=api_key)
+
+    def process_input(input):
+        st.session_state.past.append(input)
+        st.session_state.generated.append(process_prompt(input))
+
+    def load_chat(index=None, user_input=None):
+        if st.session_state['generated']:
+            # print("WE LOADING CHAT!!! \nparsed values: ", user_input, index)
+            # if user_input is not None and index is not None:
+            #     for i in range(len(st.session_state['past']) - 1, -1, -1):
+            #         if i == index:
+            #             output = process_prompt(user_input)
+            #             st.session_state["generated"].append(output)
+            #             message(st.session_state["generated"][i], key=str(i))
+            #         else:
+            #             message(st.session_state["generated"][i], key=str(i))
+            #         message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            #
+            # else:
+            for i in range(len(st.session_state['past']) - 1, -1, -1):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', seed=4)
+                message(st.session_state["generated"][i], key=str(i))
 
     # create sidebar
     with st.sidebar:
@@ -48,7 +75,7 @@ def app():
         textOutput = st.selectbox(
             "How do you want your output text?",
             ('One text file (.txt)', 'Text file per page (ZIP)'))
-        ocr_box = st.checkbox('Enable OCR (scanned document)')
+        # ocr_box = st.checkbox('Enable OCR (scanned document)')
 
         st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"), unsafe_allow_html=True)
         st.markdown("""
@@ -57,182 +84,124 @@ def app():
         """)
         st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"), unsafe_allow_html=True)
 
-
     # create tabs
-    cv_tab, career_tab, education_tab, pd_tab = st.tabs(["CV Checker", "Career Advice", "Educational Advice", "Personal Development Advice"])
+    # cv_tab = st.tabs("Career Advice")
     # only when API key is entered, render elements within tabs
     if api_key:
         # render cv tab contents
-        with cv_tab:
-            st.header("Upload your CV and we'll give our opinions!")
-            pdf_file = st.file_uploader("Load your PDF", type="pdf")
-            hide = """
+        # with cv_tab:
+        st.header("Upload your CV and we'll give our opinions!")
+        pdf_file = st.file_uploader("Load your PDF", type="pdf")
+        hide = """
+                    <style>
+                    footer{
+                        visibility: hidden;
+                            position: relative;
+                    }
+                    .viewerBadge_container__1QSob{
+                        visibility: hidden;
+                    }
+                    #MainMenu{
+                        visibility: hidden;
+                    }
+                    <style>
+                """
+        st.markdown(hide, unsafe_allow_html=True)
+
+        if pdf_file:
+            path = pdf_file.read()
+
+            # display document
+            with st.expander("Display document"):
+                displayPDF(path)
+            # if ocr_box:
+            #     option = st.selectbox('Select the document language', list(languages.keys()))
+
+            # pdf to text
+            if textOutput == 'One text file (.txt)':
+                # if ocr_box:
+                #     # poppler
+                #     texts, nb_pages = images_to_txt(path, languages[option])
+                #
+                #     totalPages = "Pages: " + str(nb_pages) + " in total"
+                #     text_data_f = "\n\n".join(texts)
+                text_data_f, nb_pages = convert_pdf_to_txt_file(pdf_file)
+                print(text_data_f)
+                totalPages = "Pages: " + str(nb_pages) + " in total"
+
+                print(text_data_f)
+                st.info(totalPages)
+
+                # st.download_button("Download txt file", text_data_f)
+                if st.button("Check my CV!"):
+                    # with st.spinner(text="In progress"):
+                    report_text = process_prompt(text_data_f, cv=True)
+                    process_input(report_text)
+                    # print(report_text)
+                    # st.markdown(report_text)
+            else:
+                # if ocr_box:
+                #     text_data, nb_pages = images_to_txt(path, languages[option])
+                #     totalPages = "Pages: " + str(nb_pages) + " in total"
+                # else:
+                text_data, nb_pages = convert_pdf_to_txt_pages(pdf_file)
+                totalPages = "Pages: " + str(nb_pages) + " in total"
+
+                st.info(totalPages)
+                zipPath = save_pages(text_data)
+                # download text data
+                with open(zipPath, "rb") as fp:
+                    btn = st.download_button(
+                        label="Download ZIP (txt)",
+                        data=fp,
+                        file_name="pdf_to_txt.zip",
+                        mime="application/zip"
+                    )
+
+            st.markdown('''
+                        <a target="_blank" style="color: black" href="">
+
+                        </a>
                         <style>
-                        footer{
-                            visibility: hidden;
-                                position: relative;
+                        .btn{
+                            display: inline-flex;
+                            -moz-box-align: center;
+                            align-items: center;
+                            -moz-box-pack: center;
+                            justify-content: center;
+                            font-weight: 400;
+                            padding: 0.25rem 0.75rem;
+                            border-radius: 0.25rem;
+                            margin: 0px;
+                            line-height: 1.6;
+                            color: rgb(49, 51, 63);
+                            background-color: #fff;
+                            width: auto;
+                            user-select: none;
+                            border: 1px solid rgba(49, 51, 63, 0.2);
+                            }
+                        .btn:hover{
+                            color: #00acee;
+                            background-color: #fff;
+                            border: 1px solid #00acee;
                         }
-                        .viewerBadge_container__1QSob{
-                            visibility: hidden;
-                        }
-                        #MainMenu{
-                            visibility: hidden;
-                        }
-                        <style>
-                    """
-            st.markdown(hide, unsafe_allow_html=True)
-
-            if pdf_file:
-                path = pdf_file.read()
-
-                # display document
-                with st.expander("Display document"):
-                    displayPDF(path)
-                if ocr_box:
-                    option = st.selectbox('Select the document language', list(languages.keys()))
-
-                # pdf to text
-                if textOutput == 'One text file (.txt)':
-                    if ocr_box:
-                        # poppler
-                        texts, nb_pages = images_to_txt(path, languages[option])
-
-                        totalPages = "Pages: " + str(nb_pages) + " in total"
-                        text_data_f = "\n\n".join(texts)
-                    else:
-                        text_data_f, nb_pages = convert_pdf_to_txt_file(pdf_file)
-                        print(text_data_f)
-                        totalPages = "Pages: " + str(nb_pages) + " in total"
-
-                    print(text_data_f)
-                    st.info(totalPages)
-                    st.download_button("Download txt file", text_data_f)
-                    if st.button("Check my CV"):
-                        # with st.spinner(text="In progress"):
-                        report_text = process_prompt(text_data_f, cv=True)
-                        print(report_text)
-                        st.markdown(report_text)
-
-                else:
-                    if ocr_box:
-                        text_data, nb_pages = images_to_txt(path, languages[option])
-                        totalPages = "Pages: " + str(nb_pages) + " in total"
-                    else:
-                        text_data, nb_pages = convert_pdf_to_txt_pages(pdf_file)
-                        totalPages = "Pages: " + str(nb_pages) + " in total"
-
-                    st.info(totalPages)
-                    zipPath = save_pages(text_data)
-                    # download text data
-                    with open(zipPath, "rb") as fp:
-                        btn = st.download_button(
-                            label="Download ZIP (txt)",
-                            data=fp,
-                            file_name="pdf_to_txt.zip",
-                            mime="application/zip"
+                        </style>
+                        ''',
+                        unsafe_allow_html=True
                         )
 
-                st.markdown('''
-                            <a target="_blank" style="color: black" href="">
+        user_input = get_text()
 
-                            </a>
-                            <style>
-                            .btn{
-                                display: inline-flex;
-                                -moz-box-align: center;
-                                align-items: center;
-                                -moz-box-pack: center;
-                                justify-content: center;
-                                font-weight: 400;
-                                padding: 0.25rem 0.75rem;
-                                border-radius: 0.25rem;
-                                margin: 0px;
-                                line-height: 1.6;
-                                color: rgb(49, 51, 63);
-                                background-color: #fff;
-                                width: auto;
-                                user-select: none;
-                                border: 1px solid rgba(49, 51, 63, 0.2);
-                                }
-                            .btn:hover{
-                                color: #00acee;
-                                background-color: #fff;
-                                border: 1px solid #00acee;
-                            }
-                            </style>
-                            ''',
-                            unsafe_allow_html=True
-                            )
+        if user_input:
+            process_input(user_input)
+            st.empty()
+        load_chat()
 
-        # render advice tab contents
-        with career_tab:
-            # set up title
-            st.title("Career Advice")
-
-            # example input
-            s_example = "I am a recent graduate. I am looking for ways to increase my chances " \
-                        "of acquiring an internship. What can I do?"
-            input = st.text_area(
-                "Tell us a little bit more about yourself! "
-                "Educational background, hobbies, personality traits, anything!",
-                value=s_example,
-                max_chars=150,
-                height=100,
-            )
-
-            if st.button("Submit", key="career"):
-                # with st.spinner(text="In progress"):
-                report_text = process_prompt(input, career=True)
-                print(report_text)
-                st.markdown(report_text)
-
-        with education_tab:
-            # set up title
-            st.title("Education Advice")
-
-            # example input
-            s_example = "I am in my final year of high school and I don't know where to go for" \
-                        " university. I am also a big animal lover. What are my options?"
-            input = st.text_area(
-                "Tell us a little bit more about yourself! "
-                "Educational background, hobbies, personality traits, anything!",
-                value=s_example,
-                max_chars=150,
-                height=100,
-            )
-
-            if st.button("Submit", key="edu"):
-                # with st.spinner(text="In progress"):
-                report_text = process_prompt(input, edu=True)
-                print(report_text)
-                st.markdown(report_text)
-
-        with pd_tab:
-            # set up title
-            st.title("Personal Development Advice")
-
-            # example input
-            s_example = "How can I increase my work productivity?"
-            input = st.text_area(
-                "Tell us a little bit more about yourself! "
-                "What's troubling you?",
-                value=s_example,
-                max_chars=150,
-                height=100,
-            )
-
-            if st.button("Submit", key="pd"):
-                # with st.spinner(text="In progress"):
-                report_text = process_prompt(input, pd=True)
-                print(report_text)
-                st.markdown(report_text)
 
     else:
         st.error("🔑 Please enter an API Key")
 
 
-
-
-
-
-
+def get_text():
+    input = st.text_input("You: ", key="input")
+    return input
--- model.py
+++ model.py
@@ -1,23 +1,5 @@
 import openai
 
-edu_prompt = """\
-You are a friendly AI acting as an education counselor. 
-Include emojis in your reply if necessary. 
-Assume that the user doesn't know what they want to pursue. 
-According to the information that the user has provided, respond with
-detailed suggestions for possible career paths and education paths 
-such as possible degrees. Arrange the data in tables and in a way that is
-readable.
-Reply in the same language as the question. Format of the reply should be 
-in markdown language, NO HTML TAGS.
-If you are unable answer the question asked, say \"I don't know\"### 
-
-I'm looking for advice.
----
-{input}
----
- """
-
 cv_prompt = """\
 Your job is to rate the CV out of 10, provide
 areas of improvement for the CV. The CV is below
@@ -28,29 +10,15 @@ areas of improvement for the CV. The CV is below
 """
 
 career_prompt = """\
-You are a friendly AI acting as an career counselor. 
-Include emojis in your reply if necessary. 
+You are a friendly career counselor AI, the user will ask you questions in areas relating
+to their career, education or personal improvement. 
+Include emoticons when appropriate.
 According to the information that the user has provided, respond with
 detailed suggestions for possible career paths or options available.
-Arrange the data in tables and in a way that is readable.
+Arrange your data in lists and structure your replies into paragraphs.
 Reply in the same language as the question. Format of the reply should be 
 in markdown language, NO HTML TAGS.
-If you are unable answer the question asked, say \"I don't know\"### 
-
-I'm looking for advice.
----
-{input}
----
- """
-
-pd_prompt = """\
-You are a friendly AI acting as an behavioural and personal development counselor. 
-Include emojis in your reply if necessary. 
-According to the information that the user has provided, respond with
-detailed/appropriate suggestions and courses of action.
-If the user is sad, console them!!!!!
-Reply in the same language as the question. Format of the reply should be 
-in markdown language, NO HTML TAGS.
+You can ask questions to further clarify the prompt.
 If you are unable answer the question asked, say \"I don't know\"### 
 
 I'm looking for advice.
@@ -96,8 +64,7 @@ class GeneralModel:
         ].strip()
         return r
 
-    def model_prediction(self, input, api_key, cv=False, career=False,
-                         edu=False, pd=False):
+    def model_prediction(self, input, api_key, cv=False):
         """
         wrapper for the API to save the prompt and the result
         """
@@ -105,13 +72,8 @@ class GeneralModel:
         set_openai_key(api_key)
 
         if cv:
-            print("CV")
             output = self.query(cv_prompt.format(input=input))
-        elif career:
+        else:
             output = self.query(career_prompt.format(input=input))
-        elif edu:
-            output = self.query(edu_prompt.format(input=input))
-        elif pd:
-            output = self.query(pd_prompt.format(input=input))
 
         return output
